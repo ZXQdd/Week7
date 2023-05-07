38cb701dccbc4e89f2e24b871aea3f06817d789f
zhao0515 <110957684+zhao0515@users.noreply.github.com>
2023-05-07 21:02:17 +0800

    commit 1

---
+++ programming_language.py
@@ -0,0 +1,32 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name="", typing="", reflection=bool, pointer_arithmetic=bool, year = 0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+    def __repr__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection},Pointer Arithmetic={self.pointer_arithmetic},First appeared in {self.year} "
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+
+def run_tests():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, False,1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, False,1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, False,1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
